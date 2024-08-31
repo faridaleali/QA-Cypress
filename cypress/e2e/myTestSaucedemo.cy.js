492890ba419c6bac4ecac975d7af3e4df843abf0
Ale Ali, Farid <faleali.contact@gmail.com>
2024-08-31 14:07:03 -0300

    Optimizacion de codigo

--- cypress/e2e/myTestSaucedemo.cy.js
+++ cypress/e2e/myTestSaucedemo.cy.js
@@ -1,60 +1,39 @@
 describe('Pruebas en compra de usuarios', () => {
-  beforeEach(() => {
-    cy.visit('https://www.saucedemo.com/')
-  });
+	let users, products, info;
 
-  it.only('Compra con user correctamente configurado', () => {
+	beforeEach(() => {
+		cy.visit('https://www.saucedemo.com/');
 
-    cy.fixture('users').then((users) => {
-      cy.login(users.standard_user.username, users.standard_user.password);
-    });
+		cy.fixture('users').then((data) => { users = data; });
+		cy.fixture('products').then((data) => { products = data.products; });
+		cy.fixture('information').then((data) => { info = data.information; });
+	});
 
-    cy.fixture('products').then((data) => {
-      data.products.forEach((products) => {
-        cy.dataTestClick(products);
-      });
-    });
+	function realizarCompra() {
+		products.forEach((product) => {
+			cy.dataTestClick(product);
+		});
 
-    cy.dataTestClick("shopping-cart-link")
-    cy.dataTestClick("checkout")
+		cy.dataTestClick("shopping-cart-link");
+		cy.dataTestClick("checkout");
 
-    cy.fixture('information').then((info) => {
-      cy.information(info.information.firstName, info.information.lastName, info.information.postalCode);
-    });
+		cy.information(info.firstName, info.lastName, info.postalCode);
 
-    cy.dataTestClick("continue")
-    cy.dataTestClick("finish")
-    cy.dataTestClick("back-to-products")
+		cy.dataTestClick("continue");
+		cy.dataTestClick("finish");
+		cy.dataTestClick("back-to-products");
 
-    cy.get('#react-burger-menu-btn').contains('Open Menu').click();
-    cy.dataTestClick("logout-sidebar-link")
-  })
+		cy.get('#react-burger-menu-btn').contains('Open Menu').click();
+		cy.dataTestClick("logout-sidebar-link");
+	}
 
-  it('Compra con user configurado erronamente', () => {
+	it('Compra con user correctamente configurado', () => {
+		cy.login(users.standard_user.username, users.standard_user.password);
+		realizarCompra();
+	});
 
-    cy.fixture('users.json').then((users) => {
-      cy.login(users.problem_user.username, users.problem_user.password);
-    })
-
-    cy.fixture('products.json').then((data) => {
-      data.products.forEach((products) => {
-        cy.dataTestClick(products);
-      });
-    });
-
-    cy.dataTestClick("shopping-cart-link")
-    cy.dataTestClick("checkout")
-
-    //Error en el lastname
-    cy.fixture('information').then((info) => {
-      cy.information(info.information.firstName, info.information.lastName, info.information.postalCode);
-    });
-
-    cy.dataTestClick("continue")
-    cy.dataTestClick("finish")
-    cy.dataTestClick("back-to-products")
-
-    cy.get('#react-burger-menu-btn').contains('Open Menu').click();
-    cy.dataTestClick("logout-sidebar-link")
-  })
-})
+	it('Compra con user configurado erronamente', () => {
+		cy.login(users.problem_user.username, users.problem_user.password);
+		realizarCompra();
+	});
+});
